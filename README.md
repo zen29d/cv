bf6af8169a494dba2c9d04449d2be50844e2065c
zen <zen@Mark.local>
2019-11-29 22:58:41 +0530

    edit readme.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# cv tools
+Computer vision tools in python
+
+* surf
+  
+    Ploting image surfce  
+
+```python
+from surf import surf
+.
+.
+surf(image)
+```
+
+![Example]
+(master/../img/example.png)
+
---
+++ surf.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2 as cv
+
+def surf(image):
+    '''
+    plot an image suraface in grayscale
+    surf(image)
+    image: 2D array
+    '''
+    # todo:  option[default:gray]= color, gray
+    # if type == 'color':
+    #     clrtype = plt.cm.jet
+    # else:
+    #     clrtype = plt.cm.gray
+
+    # Grayscaling 
+    img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # Reduing size > big size take more time to generate 
+    img = cv.resize(img, (64,64))
+
+    # Generating X and Y
+    x, y = np.mgrid[0:img.shape[0], 0:img.shape[1]]
+
+    # Ploting
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(x, y, img ,rstride=1, cstride=1, cmap=plt.cm.gray)
+    plt.show()
+
+if '__name__' != '__main__':
+    pass
